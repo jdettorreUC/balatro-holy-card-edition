885cf11868658b31264dacbc87406f4fd24fbe8d
jdettorreUC <dettorjt@mail.uc.edu>
2025-11-11 22:33:57 -0500

    joker sprites page 3

---
+++ content/jokers/page3.lua
@@ -0,0 +1,247 @@
+--Page 3 Jokers Atlas
+SMODS.Atlas{
+    key = 'HCE_Jokers3',
+    path = 'HCE_jokers3.png',
+    px = 71,
+    py = 95
+}
+
+--ID 245 (20/20)
+
+--ID 246 (Blue Map)
+
+--ID 247 (BFFS!)
+
+--ID 248 (Hive Mind)
+
+--ID 249 (There's Options)
+
+--ID 250 (Bogo Bombs)
+
+--ID 251 (Starter Deck)
+
+--ID 252 (Little Baggy)
+
+--ID 253 (Magic Scab)
+
+--ID 254 (Blood Clot)
+
+--ID 255 (Screw)
+
+--ID 256 (Hot Bombs)
+
+--ID 257 (Fire Mind)
+
+--ID 258 (Missing No.)
+
+--ID 259 (Dark Matter)
+
+--ID 260 (Black Candle)
+
+--ID 261 (Proptosis)
+
+--ID 262 (Missing Page 2)
+
+--ID 263 (Clear Rune)
+
+--ID 264 (Smart Fly)
+
+--ID 265 (Dry Baby)
+
+--ID 266 (Juicy Sack)
+
+--ID 267 (Robo-Baby 2.0)
+
+--ID 268 (Rotten Baby)
+
+--ID 269 (Headless Baby)
+
+--ID 270 (Leech)
+
+--ID 271 (Mystery Sack)
+
+--ID 272 (BBF)
+
+--ID 273 (Bob's Brain)
+
+--ID 274 (Best Bud)
+
+--ID 275 (Lil Brimstone)
+
+--ID 276 (Isaac's Heart)
+
+--ID 277 (Lil Haunt)
+
+--ID 278 (Dark Bum)
+
+--ID 279 (Big Fan)
+
+--ID 280 (Sissy Longlegs)
+
+--ID 281 (Punching Bag)
+
+--ID 282 (How To Jump)
+
+--ID 283 (D100)
+
+--ID 284 (D4)
+
+--ID 285 (D10)
+
+--ID 286 (Blank Card)
+
+--ID 287 (Book of Secrets)
+
+--ID 288 (Box of Spiders)
+
+--ID 289 (Red Candle)
+
+--ID 290 (The Jar)
+
+--ID 291 (Flush!)
+
+--ID 292 (Satanic Bible)
+
+--ID 293 (Head of Krampus)
+
+--ID 294 (Butter Bean)
+
+--ID 295 (Magic Fingers)
+
+--ID 296 (Converter)
+
+--ID 297 (Pandora's Box)
+
+--ID 298 (Unicorn Stump)
+
+--ID 299 (Taurus)
+
+--ID 300 (Aries)
+
+--ID 301 (Cancer)
+
+--ID 302 (Leo)
+
+--ID 303 (Virgo)
+
+--ID 304 (Libra)
+
+--ID 305 (Scorpio)
+
+--ID 306 (Sagittarius)
+
+--ID 307 (Capricorn)
+
+--ID 308 (Aquarius)
+
+--ID 309 (Pisces)
+
+--ID 310 (Eve's Mascara)
+
+--ID 311 (Judas' Shadow)
+
+--ID 312 (Maggy's Bow)
+
+--ID 313 (Holy Mantle)
+
+--ID 314 (Thunder Thighs)
+
+--ID 315 (Strange Attractor)
+
+--ID 316 (Cursed Eye)
+
+--ID 317 (Mysterious Liquid)
+
+--ID 318 (Gemini)
+
+--ID 319 (Cain's Other Eye)
+
+--ID 320 (???'s Only Friend)
+
+--ID 321 (Samson's Chains)
+
+--ID 322 (Mongo Baby)
+
+--ID 323 (Isaac's Tears)
+
+--ID 324 (Undefined)
+
+--ID 325 (Scissors)
+
+--ID 326 (Breath of Life)
+
+--ID 327 (The Polaroid)
+
+--ID 328 (The Negative)
+
+--ID 329 (Ludovico Technique)
+
+--ID 330 (Soy Milk)
+
+--ID 331 (Godhead)
+
+--ID 332 (Lazarus' Rags)
+
+--ID 333 (The Mind)
+
+--ID 334 (The Body)
+
+--ID 335 (The Soul)
+
+--ID 336 (Dead Onion)
+
+--ID 337 (Broken Watch)
+
+--ID 338 (The Boomerang)
+
+--ID 339 (Safety Pin)
+
+--ID 340 (Caffeine Pill)
+
+--ID 341 (Torn Photo)
+
+--ID 342 (Blue Cap)
+
+--ID 343 (Latch Key)
+
+--ID 344 (Match Book)
+
+--ID 345 (Synthoil)
+
+--ID 346 (A Snack)
+
+--ID 347 (Diplopia)
+
+--ID 348 (Placebo)
+
+--ID 349 (Wooden Nickel)
+
+--ID 350 (Toxic Shock)
+
+--ID 351 (Mega Bean)
+
+--ID 352 (Glass Cannon) ***SPRITE IS MISPELLED, NEEDS FIXED***
+
+--ID 353 (Bomber Boy)
+
+--ID 354 (Crack Jacks)
+
+--ID 355 (Mom's Pearls)
+
+--ID 356 (Car Battery)
+
+--ID 357 (Box of Friends)
+
+--ID 358 (The Wiz)
+
+--ID 359 (8 Inch Nails)
+
+--ID 360 (Incubus)
+
+--ID 361 (Fate's Reward)
+
+--ID 362 (Lil Chest)
+
+--ID 363 (Sworn Protector)
+
+--ID 364 (Friend Zone)
